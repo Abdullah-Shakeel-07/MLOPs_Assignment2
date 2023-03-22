630ffa74c4f6a0b0ef0c263dc45c70db2b0242b8
Abdullah-Shakeel-07 <imabdullah.shakeel@gmail.com>
2023-03-22 22:32:02 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import pickle
+from datetime import datetime
+import numpy as np
+# import pandas as pd
+# from sklearn.metrics import mean_absolute_error
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("frontend.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=np.array(int_features)
+    y_pred = model.predict(final.reshape(1, -1))
+    # print(int(y_pred))
+    return render_template('frontend.html',pred='Predcited price is {}'.format(y_pred))
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
